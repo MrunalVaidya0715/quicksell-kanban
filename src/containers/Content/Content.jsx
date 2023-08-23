8ddc82eb19a4fcd0e1b39578ab43c52ec31a2c80
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-08-24 02:27:58 +0530

    Ordering and Sorting Success

--- src/containers/Content/Content.jsx
+++ src/containers/Content/Content.jsx
@@ -5,9 +5,13 @@ import TicketCard from '../../components/TicketCard/TicketCard';
 const Content = () => {
 
     const userViewState = JSON.parse(localStorage.getItem("userViewState"));
+    const groupBy = userViewState.groupBy;
+    console.log(groupBy);
+    const orderBy = userViewState.orderBy;
+    console.log(orderBy);
     const [ticketsData, setTicketsData] = useState([]);
     const [usersData, setUsersData] = useState([]);
-    
+
     useEffect(() => {
         fetch("https://api.quicksell.co/v1/internal/frontend-assignment")
             .then((response) => response.json())
@@ -18,14 +22,44 @@ const Content = () => {
             .catch((error) => console.error(error));
     }, []);
 
+    const groupedAndSortedTickets = () => {
 
+        const groupedTickets = {};
+        ticketsData.forEach(ticket => {
+            const groupKey = groupBy === 'user' ? ticket.userId : groupBy === 'priority'? ticket.priority: ticket.status;
+            if (!groupedTickets[groupKey]) {
+                groupedTickets[groupKey] = [];
+            }
+            groupedTickets[groupKey].push(ticket);
+        });
+
+        for (const key in groupedTickets) {
+            groupedTickets[key].sort((a, b) => {
+                if (orderBy === 'priority') {
+                    return b.priority - a.priority;
+                } else {
+                    return a.title.localeCompare(b.title);
+                }
+            });
+        }
+
+        return groupedTickets;
+    };
+
+    const groupedAndSorted = groupedAndSortedTickets();
     return (
         <div className='content'>
             <div className='grids'>
-                {ticketsData.map((ticket) => (
-                    <TicketCard key={ticket.id} {...ticket} />
+                {Object.keys(groupedAndSorted).map(groupKey => (
+                    <div key={groupKey}>
+                        <h2>{groupKey}</h2>
+                        {groupedAndSorted[groupKey].map(ticket => (
+                            <TicketCard key={ticket.id} {...ticket} />
+                        ))}
+                    </div>
                 ))}
             </div>
+
         </div>
     );
 }
