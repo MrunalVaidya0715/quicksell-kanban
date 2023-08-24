99428ecad13210937e16b934d3d807287c351515
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-08-24 12:48:23 +0530

    Display Label

---
+++ src/components/Label/Label.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Label.css"
+const Label = ({labelText}) => {
+  return (
+    <div>
+        <p>{labelText}</p>
+    </div>
+  )
+}
+
+export default Label
--- src/containers/Content/Content.jsx
+++ src/containers/Content/Content.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import './Content.css';
 import TicketCard from '../../components/TicketCard/TicketCard';
+import Label from '../../components/Label/Label';
 
 const Content = () => {
 
     const userViewState = JSON.parse(localStorage.getItem("userViewState"));
     const groupBy = userViewState.groupBy;
-    console.log(groupBy);
     const orderBy = userViewState.orderBy;
-    console.log(orderBy);
+    console.log(groupBy,"-",orderBy);
     const [ticketsData, setTicketsData] = useState([]);
     const [usersData, setUsersData] = useState([]);
 
@@ -22,6 +22,7 @@ const Content = () => {
             .catch((error) => console.error(error));
     }, []);
 
+
     const groupedAndSortedTickets = () => {
 
         const groupedTickets = {};
@@ -34,7 +35,7 @@ const Content = () => {
         });
 
         for (const key in groupedTickets) {
-            groupedTickets[key].sort((a, b) => {
+            groupedTickets[key].sort((a,b) => {
                 if (orderBy === 'priority') {
                     return b.priority - a.priority;
                 } else {
@@ -47,12 +48,37 @@ const Content = () => {
     };
 
     const groupedAndSorted = groupedAndSortedTickets();
+
+    const getLabelForGroupKey = (groupKey) => {
+        if (groupBy === 'priority') {
+            const priorityLabels = {
+                4: 'Urgent',
+                3: 'High',
+                2: 'Medium',
+                1: 'Low',
+                0: 'No priority'
+            };
+            return priorityLabels[groupKey];
+        } else if (groupBy === 'status') {
+            const statusLabels = {
+                'Todo': 'To Do',
+                'In progress': 'In Progress',
+                'Backlog': 'Backlog',
+                'Completed': 'Completed',
+                'Cancelled': 'Cancelled'
+            };
+            return statusLabels[groupKey];
+        } else if (groupBy === 'user') {
+            const user = usersData.find(user => user.id === groupKey);
+            return user ? user.name : '';
+        }
+    };
     return (
         <div className='content'>
             <div className='grids'>
                 {Object.keys(groupedAndSorted).map(groupKey => (
                     <div key={groupKey}>
-                        <h2>{groupKey}</h2>
+                        <Label labelText={getLabelForGroupKey(groupKey)}/>
                         {groupedAndSorted[groupKey].map(ticket => (
                             <TicketCard key={ticket.id} {...ticket} />
                         ))}
