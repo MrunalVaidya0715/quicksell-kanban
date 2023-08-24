2d428d84e503fb073c56958d6f826f1560743d30
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-08-24 16:50:01 +0530

    CR icons

--- src/components/TicketCard/TicketCard.jsx
+++ src/components/TicketCard/TicketCard.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import './TicketCard.css';
-import { BsExclamationSquareFill } from 'react-icons/bs'
-const TicketCard = ({ id, title, userImg, tag, userId }) => {
+import { PiCheckCircleFill, PiCircle, PiClockCountdownBold, PiSpinnerLight } from 'react-icons/pi';
+import { BsFillExclamationSquareFill } from 'react-icons/bs'
+import { MdSignalCellularAlt, MdSignalCellularAlt2Bar, MdSignalCellularAlt1Bar, MdCancel } from 'react-icons/md'
+import { CgBorderStyleDashed } from 'react-icons/cg';
+import UserImg from '../UserImg/UserImg';
+import { useUserViewState } from '../../context/UserViewStateContext';
+
+const TicketCard = ({ id, title, userImg, tag, status, priority }) => {
+    const { userViewState } = useUserViewState();
+    const { groupBy, orderBy } = userViewState;
+
     return (
         <div className='ticketCard'>
             {/* Info */}
@@ -11,13 +20,34 @@ const TicketCard = ({ id, title, userImg, tag, userId }) => {
                         <p>{id}</p>
                     </div>
                     <div className='title'>
+                        {
+                            !(groupBy === "status") && (
+                                <div>
+                                    {
+                                        status === "Backlog" ? <PiSpinnerLight style={{ color: 'gray' }} /> :
+                                            status === "In progress" ? <PiClockCountdownBold style={{ color: '#FFCC4D' }} /> :
+                                                status === "Completed" ? <PiCheckCircleFill style={{ color: 'blue' }} /> :
+                                                    status === "Cancelled" ? <MdCancel style={{ color: 'gray' }} /> : <PiCircle style={{ color: 'gray' }} />
+                                    }
+                                </div>
+                            )
+                        }
                         <p>{title}</p>
                     </div>
                 </div>
                 <div className='tags'>
-                    <div className='status'>
-                        <BsExclamationSquareFill />
-                    </div>
+                    {
+                        !(groupBy === "priority") && (
+                            <div className='status'>
+                                {
+                                    priority === 4 ? <BsFillExclamationSquareFill style={{ color: 'red' }} /> :
+                                        priority === 3 ? <MdSignalCellularAlt style={{ color: 'darkslategrey' }} /> :
+                                            priority === 2 ? <MdSignalCellularAlt2Bar style={{ color: 'darkslategrey' }} /> :
+                                                priority === 1 ? <MdSignalCellularAlt1Bar /> : <CgBorderStyleDashed style={{ color: 'darkslategrey' }} />
+                                }
+                            </div>
+                        )
+                    }
                     <div className=' tag'>
                         <div className='circle' />
                         {
@@ -27,10 +57,9 @@ const TicketCard = ({ id, title, userImg, tag, userId }) => {
                 </div>
             </div>
             {/* UserImg */}
-            <div className='userImg'>
-                <img src="/assets/userPic.png" alt={userImg} />
-                <div className='available active' />
-            </div>
+            {
+                !(groupBy === "user") && <UserImg />
+            }
         </div>
     )
 }
